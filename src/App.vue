6b32cbdc737055b03bb9dbb6d8eac7191cc5e75d
pot-Space <medey4work@gmail.com>
2024-09-30 17:52:34 +0300

    adjustments

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,45 @@
 <template>
-  <div class="todo">
-    <h1 class="todo__headline">TODO</h1>
-    
-    <input 
-      class="todo__task-input" 
-      v-model="newTask" 
-      @keyup.enter="addTask" 
-      type="text" 
-      placeholder="Новая задача"
-    />
-    
-    <ul class="todo__list">
-      <li 
-      class="todo__list-item" 
-      v-for="task in tasks" 
-      :key="task.id" 
-      :class="{ completed: task.completed }"
-      >
+	<div class="todo">
+		<h1 class="todo__headline">TODO</h1>
 
-        <input 
-          class="todo__btn-complete" 
-          type="checkbox" 
-          v-model="task.completed" 
-          @change="updateLocalStorage" 
-        />
+		<input
+			class="todo__task-input"
+			v-model="newTask"
+			@keyup.enter="addTask"
+			type="text"
+			placeholder="Новая задача"
+		/>
 
-        <span class="todo__text" @dblclick="editTask(task)">{{ task.text }}</span>
+		<ul class="todo__list">
+			<li
+				class="todo__list-item"
+				v-for="task in tasks"
+				:key="task.id"
+				:class="{ completed: task.completed }"
+			>
+				<input
+					class="todo__btn-complete"
+					type="checkbox"
+					v-model="task.completed"
+					@change="updateLocalStorage"
+				/>
 
-        <textarea
-          class="todo__text-input"
-          v-if="editingTask && editingTask.id === task.id" 
-          v-model="editingTask.text" 
-          @keyup.enter="saveTask" 
-          @blur="saveTask" 
-        />
+				<span class="todo__text" @dblclick="editTask(task)">{{
+					task.text
+				}}</span>
 
-        <button class="todo__btn-delete" @click="deleteTask(task.id)">✘</button>
-      </li>
-    </ul>
-  </div>  
+				<textarea
+					class="todo__text-input"
+					v-if="editingTask && editingTask.id === task.id"
+					v-model="editingTask.text"
+					@keyup.enter="saveTask"
+					@blur="saveTask"
+				/>
+
+				<button class="todo__btn-delete" @click="deleteTask(task.id)">✘</button>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script setup>
@@ -49,47 +50,53 @@ const newTask = ref('')
 const editingTask = ref(null)
 
 onMounted(() => {
-  const storedTasks = localStorage.getItem('tasks')
-  if (storedTasks) {
-    tasks.value = JSON.parse(storedTasks)
-  }
+	const storedTasks = localStorage.getItem('tasks')
+	if (storedTasks) {
+		tasks.value = JSON.parse(storedTasks)
+	}
 })
 
-watch(tasks, (newTasks) => {
-  localStorage.setItem('tasks', JSON.stringify(newTasks))
-}, { deep: true })
+watch(
+	tasks,
+	newTasks => {
+		localStorage.setItem('tasks', JSON.stringify(newTasks))
+	},
+	{ deep: true }
+)
 
 const addTask = () => {
-  if (newTask.value.trim()) {
-    tasks.value.push({
-      id: Date.now(),
-      text: newTask.value,
-      completed: false
-    })
-    newTask.value = ''
-  }
+	if (newTask.value.trim()) {
+		tasks.value.push({
+			id: Date.now(),
+			text: newTask.value,
+			completed: false,
+		})
+		newTask.value = ''
+	}
 }
 
-const deleteTask = (id) => {
-  tasks.value = tasks.value.filter(task => task.id !== id)
+const deleteTask = id => {
+	tasks.value = tasks.value.filter(task => task.id !== id)
 }
 
-const editTask = (task) => {
-  editingTask.value = { ...task }
+const editTask = task => {
+	editingTask.value = { ...task }
 }
 
 const saveTask = () => {
-  if (editingTask.value) {
-    const taskIndex = tasks.value.findIndex(task => task.id === editingTask.value.id)
+	if (editingTask.value) {
+		const taskIndex = tasks.value.findIndex(
+			task => task.id === editingTask.value.id
+		)
 
-    if ((taskIndex !== -1) && (editingTask.value.text.trim().length > 0)) {
-      tasks.value[taskIndex].text = editingTask.value.text
-    }
-    editingTask.value = null
-  }
+		if (taskIndex !== -1 && editingTask.value.text.trim().length > 0) {
+			tasks.value[taskIndex].text = editingTask.value.text
+		}
+		editingTask.value = null
+	}
 }
 
 const updateLocalStorage = () => {
-  localStorage.setItem('tasks', JSON.stringify(tasks.value))
+	localStorage.setItem('tasks', JSON.stringify(tasks.value))
 }
 </script>
